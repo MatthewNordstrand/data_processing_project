c569c4c93766d934173ad0f05be840e0d756485a
Nordstrand <mnordstrand@teksystems.com>
2023-04-17 11:12:40 -0400

    Changed order between DataIAM and Bucket creation

--- lib/matt_project_data_processing-stack.ts
+++ lib/matt_project_data_processing-stack.ts
@@ -7,8 +7,8 @@ export class MattProjectDataProcessingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new DataIAM(this, "DataIAM");
-
     new Bucket(this, "Bucket");
+
+    new DataIAM(this, "DataIAM");
   }
 }
