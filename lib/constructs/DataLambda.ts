894848b4eaa8eb75c1b682e976cecb0830a4bd6a
Nordstrand <mnordstrand@teksystems.com>
2023-04-18 13:37:54 -0400

    Added Data transformation Lambda function

--- lib/constructs/DataLambda.ts
+++ lib/constructs/DataLambda.ts
@@ -1,7 +1,14 @@
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 export default class DataLambda extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    new NodejsFunction(this, "Function", {
+      entry: join(__dirname, "..", "lambda-functions", "FunctionDataLambda.ts"),
+      handler: "handler",
+    });
   }
 }
