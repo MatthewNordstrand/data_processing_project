730ae8be1fd7a2ab0ba8125bc9f300fd4025873d
Nordstrand <mnordstrand@teksystems.com>
2023-04-20 14:26:59 -0400

    Added code to lambda showing how it AWS SDK can be imported for now

--- lib/lambda-functions/FunctionDataLambda.ts
+++ lib/lambda-functions/FunctionDataLambda.ts
@@ -1,6 +1,12 @@
+import { Firehose } from "aws-sdk";
+
 async function handler(event: any, context: any) {
   console.log(`Event: ${JSON.stringify(event)}`);
   console.log(`Context: ${JSON.stringify(context)}`);
+
+  // Yeeeeeahhhh! Progress!
+  // const firehose = new Firehose();
+  // firehose.putRecord(); //This call will put data into the stream.
 
   return {
     statusCode: 200,
