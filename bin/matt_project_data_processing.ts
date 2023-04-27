7eae1d1d6b0a32c70b21e149ae814562c53f840d
Nordstrand <mnordstrand@teksystems.com>
2023-04-27 09:32:41 -0400

    Added streamname function to make other calls to the lambda faster

--- bin/matt_project_data_processing.ts
+++ bin/matt_project_data_processing.ts
@@ -4,9 +4,4 @@ import * as cdk from "aws-cdk-lib";
 import { MattProjectDataProcessingStack } from "../lib/matt_project_data_processing-stack";
 
 const app = new cdk.App();
-new MattProjectDataProcessingStack(app, "MattProjectDataProcessingStack", {
-  env: {
-    account: "211512247116",
-    region: "us-east-1",
-  },
-});
+new MattProjectDataProcessingStack(app, "MattProjectDataProcessingStack");
